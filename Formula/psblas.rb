ce505413727e603a9ce083c4ee8e9c621c19a76a
Izaak Beekman <zbeekman@gmail.com>
2017-10-25 22:15:54 -0400

    psblas: various fixes and tweaks
     - Add head install
     - Make METIS optional
     - Add option for long ints (still broken in psblas)
     - Apply patch for `make check`

--- Formula/psblas.rb
+++ Formula/psblas.rb
@@ -7,13 +7,21 @@ class Psblas < Formula
   url "https://github.com/sfilippone/psblas3/archive/v3.5.0.tar.gz"
   sha256 "2c110c5382e4ba344c92e193e13432e860b278f240b4d3657b7b2c21b8e3f64a"
 
-  option "with-test", "Run build-time tests"
+  head "https://github.com/sfilippone/psblas3.git"
+
+  option "without-test", "Skip build-time tests (not recommended)"
+  option "with-long-ints", "Build with 64 bit integers for > 2GB index space"
 
   depends_on :fortran
   depends_on :mpi => [:cc, :f90, :recommended]
   depends_on "openblas" => OS.mac? ? :optional : :recommended
   depends_on "veclibfort" if build.without?("openblas") && OS.mac?
-  depends_on "metis" => :recommended
+  depends_on "metis" => :optional
+
+  patch do
+    url "https://github.com/sfilippone/psblas3/pull/1.patch?full_index=1"
+    sha256 "cba630879d1c27f20429ab43a4c623b65bf4e78d491ec09254ab9d43d8c4fbb1"
+  end
 
   def install
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
@@ -30,6 +38,7 @@ class Psblas < Formula
       args += ["--with-blas=-lblas", "--with-lapack=-llapack"]
     end
     args << "--enable-serial" if build.without? "mpi"
+    args << "--enable-long-integers" if build.with? "long-ints"
     system "./configure", *args
     system "make", "install"
     system "make", "check" if build.with? "test"
