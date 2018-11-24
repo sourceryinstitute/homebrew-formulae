d1932ee35c30227a8e1afbb81f1a0d275b9b1a74
Izaak Beekman <zbeekman@gmail.com>
2018-11-24 12:10:28 -0500

    hwloc: build with cairo and x11

---
+++ Formula/hwloc.rb
@@ -0,0 +1,35 @@
+class Hwloc < Formula
+  desc "Portable abstraction of the hierarchical topology of modern architectures"
+  homepage "https://www.open-mpi.org/projects/hwloc/"
+  url "https://www.open-mpi.org/software/hwloc/v2.0/downloads/hwloc-2.0.2.tar.bz2"
+  sha256 "14457d70e6f98ee9eb3f2940000da4bac99909a49560ef2fdf4eacd286410cde"
+
+  head do
+    url "https://github.com/open-mpi/hwloc.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on :x11
+  depends_on "cairo"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-shared",
+                          "--enable-static",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+
+    pkgshare.install "tests"
+  end
+
+  test do
+    system ENV.cc, pkgshare/"tests/hwloc/hwloc_groups.c", "-I#{include}",
+                   "-L#{lib}", "-lhwloc", "-o", "test"
+    system "./test"
+  end
+end
